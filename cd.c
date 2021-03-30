e991931394f67526e78b3edd96edc84b77e6e15c
jontxu13 <71375283+jontxu13@users.noreply.github.com>
2021-03-30 11:11:54 +0200

    Add files via upload

--- cd.c
+++ cd.c
@@ -1,77 +1,15 @@
-#include <stdio.h>
-
-#include <sys/stat.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <dirent.h>
-//#include <error.h>
-#include "ls.c"
-
-int hasPrefix(char const *, char const *);
-int Cd(char *pth);
-
-#define BUFFERSIZE 200
-int main(){
-
-    char buffer[BUFFERSIZE];
-    char *prompt = "OS";
-    char *a = ">";
-
-    char *tok;
-    tok = strtok (buffer," ");
-
-
-    while(buffer != NULL){
-        bzero(buffer, BUFFERSIZE);
-        printf("%s%s",prompt,a);
-        fgets(buffer, BUFFERSIZE, stdin);
-        if(hasPrefix(buffer,"cd") == 0){
-            tok = strchr(buffer,' '); //use something more powerful
-            if(tok) {
-                char *tempTok = tok + 1;
-                tok = tempTok;
-                char *locationOfNewLine = strchr(tok, '\n');
-                if(locationOfNewLine) {
-                    *locationOfNewLine = '\0';
-                }
-                Cd(tok);
-            }
+#include<stdio.h>
+#include<unistd.h>
+int main(int argc, char **argv)
+{
+    if(argc<2){
+        printf("Need more parameter, Ex: 'cd root'\n");
+    }else{
+        if (chdir(argv[1])==-1){
+            return -1;
         }else{
-            if(hasPrefix(buffer,"exit")==0){
-                exit(0);
-            }
-
+            chdir(argv[1]);
         }
     }
-    return 0;
-}
-
-int hasPrefix(char const *p, char const *q)
-{
-    int i = 0;
-    for(i = 0;q[i];i++)
-    {
-        if(p[i] != q[i])
-            return -1;
-    }
-    return 0;
-}
-
-int Cd(char *pth){
-    char path[BUFFERSIZE];
-    strcpy(path,pth);
-
-    char cwd[BUFFERSIZE];
-    if(pth[0] != '/')
-    {// true for the dir in cwd
-        getcwd(cwd,sizeof(cwd));
-        strcat(cwd,"/");
-        strcat(cwd,path);
-        chdir(cwd);
-    }else{//true for dir w.r.t. /
-        chdir(pth);
-    }
-
     return 0;
 }
