10924efa10dc17703b9c0df58bf5628477a2931d
TheBirb <60443461+TheBirb@users.noreply.github.com>
2021-04-18 14:58:31 +0200

    Add files via upload

--- ls.c
+++ ls.c
@@ -11,6 +11,7 @@
 #include<pwd.h>
 #include <grp.h>
 #include <string.h>
+#include <errno.h>
 int main(int argc, char *argv[]){
     DIR *dir;
     struct dirent *currentDir; //current directory
@@ -21,7 +22,6 @@ int main(int argc, char *argv[]){
     char *c;
     int i;
     int type;
-
 
     if(argc <1) {
 
@@ -44,14 +44,20 @@ int main(int argc, char *argv[]){
         }else {
             if ((dir = opendir(argv[1])) == NULL) {
                 perror("Couldn't open directory");
+
+
                 return -1;
             }
 
             type = 1;
         }
     }else{
+
         if((dir=opendir(argv[2]))==NULL){
+            printf("%s\n", strerror(errno));
             perror("Couldn't open directory");
+
+            printf("Oh dear, something went wrong with read()! %s\n", strerror(errno));
             return -1;
         }
 
@@ -64,26 +70,33 @@ int main(int argc, char *argv[]){
             printf("%s\n",currentDir->d_name);
         }
     }else{
+        char currentPath[200];
+        char programPath[200];
+        getcwd(currentPath, sizeof currentPath);
+        sprintf(currentPath, "%s/%s/", currentPath, argv[2]);
         while((currentDir=readdir(dir))!=NULL){
 
             buf = currentDir->d_name;
             if(strcmp(buf, ".")==0 ||strcmp(buf, "..")==0 ){
 
             }else{
-                stat(buf, &thestat);
+                sprintf(programPath, "%s%s", currentPath, buf);
+                stat(programPath, &thestat);
+
                 switch (thestat.st_mode & S_IFMT) {
-                    case S_IFBLK:  printf("b "); break;
-                    case S_IFCHR:  printf("c "); break;
-                    case S_IFDIR:  printf("d "); break; //It's a (sub)directory
-                    case S_IFIFO:  printf("p "); break; //fifo
-                    case S_IFLNK:  printf("l "); break; //Symbolic link
-                    case S_IFSOCK: printf("s "); break;
+                    case S_IFBLK:  printf("b"); break;
+                    case S_IFCHR:  printf("c"); break;
+                    case S_IFDIR:  printf("d"); break; //It's a (sub)directory
+                    case S_IFIFO:  printf("p"); break; //fifo
+                    case S_IFLNK:  printf("l"); break; //Symbolic link
+                    case S_IFSOCK: printf("s"); break;
                         //Filetype isn't identified
-                    default:       printf("- "); break;
+                    default:       printf("-"); break;
                 }
+
                 //[permissions]
                 //Same for the permissions, we have to test the different right
-                printf( (thestat.st_mode & S_IRUSR) ? " r" : " -");
+                printf( (thestat.st_mode & S_IRUSR) ? "r" : "-");
                 printf( (thestat.st_mode & S_IWUSR) ? "w" : "-");
                 printf( (thestat.st_mode & S_IXUSR) ? "x" : "-");
                 printf( (thestat.st_mode & S_IRGRP) ? "r" : "-");
@@ -92,23 +105,24 @@ int main(int argc, char *argv[]){
                 printf( (thestat.st_mode & S_IROTH) ? "r" : "-");
                 printf( (thestat.st_mode & S_IWOTH) ? "w" : "-");
                 printf( (thestat.st_mode & S_IXOTH) ? "x" : "-");
+                printf(" ");
 
                 // [number of hard links]
                 // "This count keeps track of how many directories have entries for this file.
                 // If the count is ever decremented to zero, then the file itself is discarded as soon as no process still holds it open."
-                printf("\t%d ", thestat.st_nlink);
+                printf("%d ", thestat.st_nlink);
 
                 //[owner]
                 owner = getpwuid(thestat.st_uid);
-                printf("\t%s ", owner->pw_name);
-
+                printf("%s ", owner->pw_name);
                 //[group]
                 group = getgrgid(thestat.st_gid);
-                printf("\t%s ", group->gr_name);
+                printf("%s ", group->gr_name);
 
                 //size, name and date
-                printf("%zu",thestat.st_size);
+                printf("%4d",thestat.st_size);
                 printf(" %s", currentDir->d_name);
+
                 printf(" %s", ctime(&thestat.st_mtime));
             }
 
